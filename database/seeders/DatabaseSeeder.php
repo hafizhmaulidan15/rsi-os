95192f16d8a0f2a219d1e302a15f1519769e52d1
Muhammad Hafizh Maulidan <hafizh15603@gmail.com>
2026-06-22 22:56:51 +0700

    Fix seeder: user hafizh@rsi.com + db:seed in start command

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,10 +9,14 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Hafizh',
-            'email' => 'hafizh@rumahsusu.id',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'hafizh@rsi.com'],
+            [
+                'name' => 'Hafizh',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now(),
+            ]
+        );
 
         $this->call([
             SupplierSeeder::class,
